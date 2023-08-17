df6caf13a01ffbf562221d63a368e2af0929c458
Kevin-Prichard <kprichard@gmail.com>
2023-08-17 13:05:15 -0700

    - csv2db.py: rename --file/-f param to --zip/-z   - zip_walker:     - output_dir: removed     - name_filter_rx: added (zip member filtering regex), for skipping archive member names other than spec'd     - max_files: removed     - max_rows: default None     - output_fn: default None - TODO.md: moved to Issues - .gitignore: ignore .ai dir from a plugin

--- csv2db.py
+++ csv2db.py
@@ -35,7 +35,7 @@ def get_args(argv: List[str]) -> Tuple[argparse.Namespace, ArgumentParser]:
         prog='csv2db.py',
         description = 'CSV Schema Generator:'
                       'extracts the top -n rows from .CSV files in a .ZIP archive.')
-    parser.add_argument('--file', '-f', dest='file', type=str, action='store')
+    parser.add_argument('--zip', '-z', dest='zip_file', type=str, action='store')
     parser.add_argument('--dir', '-d', dest='output_dir', type=str, action='store')
     parser.add_argument('--limit', '-l', dest='max_csv_files', default=0, type=int, action='store', nargs='?')
     parser.add_argument('--max', '-n', dest='max_csv_rows', default=0, type=int, action='store', nargs='?')
@@ -49,7 +49,8 @@ def get_table_lengths(table_len_csv_fh):
     return {row[0]: int(row[1]) for row in rdr}
 
 
-def zip_walker(zip_filename, output_dir, max_files, max_rows, output_fn):
+def zip_walker(zip_filename, name_filter_rx: re.Pattern=None,
+               max_rows=None, output_fn=None):
     with zipfile.ZipFile(zip_filename, "r") as zip:
         zip_name = os.path.basename(zip_filename)
         files_done = 0
@@ -57,6 +58,8 @@ def zip_walker(zip_filename, output_dir, max_files, max_rows, output_fn):
         table_lengths = get_table_lengths(zip.open(table_lengths_filename))
         table_sql = dict()
         for file_no, name in enumerate(zip.namelist()):
+            if name_filter_rx and not name_filter_rx.match(name):
+                continue
             table_name = name.split(".")[0]
             # if table_name not in ('acquisition_samples', 'fndds_derivation'):
             #     continue
@@ -179,8 +182,8 @@ def main(argv=None):
     create_fn = None
     if args.sqlite_db_file:
         create_fn = partial(create_import_sqlite, args.sqlite_db_file)
-    if args.file:
-        zip_walker(args.file, args.output_dir, args.max_csv_files,
+    if args.zip_file:
+        zip_walker(args.zip_file, args.output_dir, args.max_csv_files,
                    args.max_csv_rows, create_fn)
     else:
         parser.print_help()
